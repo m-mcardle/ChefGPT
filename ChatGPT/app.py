f55cf492426ce49cd2f3b26178f63498b20f98ea
Matt McArdle <matthew.j.mcardle@gmail.com>
2023-03-25 11:28:37 -0400

    Initial commit

---
+++ ChatGPT/app.py
@@ -0,0 +1,12 @@
+import os
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# Load your API key from an environment variable or secret management service
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
+print(response)
